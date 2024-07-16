7ff92d319b60c01f976115e7582a108a6b8420e9
Lys-is <nikneup@yandex.ru>
2024-07-16 20:35:46 +0300

    у

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -1,7 +1,6 @@
 const {Schema, model} = require('mongoose');
 
 const UserSchema = new Schema({
-    nickname: {type: String, unique: true},
     email: {type: String, unique: true, required: true},
     password: {type: String, required: true},
     tel: {type: String},
