8fcf32f918998ab561b5a00ef1f46af0c1898f90
Lys-is <nikneup@yandex.ru>
2024-09-25 13:58:38 +0300

    ва

--- server/service/mail-service.js
+++ server/service/mail-service.js
@@ -6,7 +6,10 @@ class MailService {
         this.transporter = nodemailer.createTransport({
             host: 'smtp.beget.com',
             port: '2525',
-            secure: true,
+            secureConnection: false,
+            tls: {
+                ciphers: "SSLv3",
+              },
             auth: {
                 user: 'sporlive@sporlive.ru',
                 pass: 'dgm&RSKB8oHy'
@@ -15,19 +18,24 @@ class MailService {
     }
 
     async sendMail(to, link) {
-        await this.transporter.sendMail({
-            from: 'sporlive@sporlive.ru',
-            to,
-            subject: 'Восстановление пароля',
-            text: '',
-            html:
-                `
-                    <div>
-                        <h1>Для активации перейдите по ссылке</h1>
-                        <a href="${link}">${link}</a>
-                    </div>
-                `
-        })
+        try {
+            await this.transporter.sendMail({
+                from: 'sporlive@sporlive.ru',
+                to,
+                subject: 'Восстановление пароля',
+                text: '',
+                html:
+                    `
+                        <div>
+                            <h1>Для активации перейдите по ссылке</h1>
+                            <a href="${link}">${link}</a>
+                        </div>
+                    `
+            })
+        } catch (e) {
+            throw e
+        }
+        
     }
 }
 
